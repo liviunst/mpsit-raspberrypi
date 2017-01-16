75f7495a510180c036409a9d015938ed550e2075
liviunst <liviunastase24@gmail.com>
2017-01-16 23:46:08 +0200

    add server support

--- RaspberryDashboard/RaspberryDashboard/MainPage.xaml.cs
+++ RaspberryDashboard/RaspberryDashboard/MainPage.xaml.cs
@@ -4,12 +4,15 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Media.Capture;
 using Windows.Media.MediaProperties;
 using Windows.Storage;
 using Windows.Storage.Streams;
+using Windows.System.Threading;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -29,25 +32,80 @@ namespace RaspberryDashboard
     public sealed partial class MainPage : Page
     {
         private HttpClient _client = new HttpClient();
-        private string _serverIp;
         private List<Image> images = new List<Image>();
         private MediaCapture _mediaCapture;
         private int _currentImageId;
         private List<Record> _records = new List<Record>();
+        private object _thisLock = new object();
+        private int _iteration = 0;
+
+        private string _serverIpValue;
+        private string ServerIpValue
+        {
+            get
+            {
+                lock (_thisLock)
+                {
+                    return _serverIpValue;
+                }
+            }
+            set
+            {
+                lock (_thisLock)
+                {
+                    _serverIpValue = value;
+                }
+            }
+        }
 
         public MainPage()
         {
             this.InitializeComponent();
+        }
 
+        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
             InitializeWebCam();
 
             images.Add(Image1);
             images.Add(Image2);
             images.Add(Image3);
+
+            InitializeServerCommunication(Dispatcher);
         }
 
+        private void InitializeServerCommunication(CoreDispatcher dispatcher)
+        {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            ThreadPool.RunAsync(async asyncAction => {
+                while (true)
+                {
+                    try
+                    {
+                        if (!string.IsNullOrEmpty(ServerIpValue))
+                        {
+                            var address = "http://" + ServerIpValue + ":9000/api/rasp/command";
+                            var response = await _client.GetAsync(address);
+                            var result = await response.Content.ReadAsStringAsync();
+                            dispatcher.TryRunAsync(CoreDispatcherPriority.High, () =>
+                            {
+                                if (_iteration % 5 == 0) Logging.Text = "";
+                                _iteration++;
+                                Logging.Text += DateTime.Now + " --- " + result + " --- " + Environment.NewLine;
+                            });
+                        }
+                        Task.Delay(1000).Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        var message = ex.Message;
+                    }
+                }
+            });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
 
-#region WebCam
+        #region WebCam
 
         private async void InitializeWebCam()
         {
@@ -104,7 +162,9 @@ namespace RaspberryDashboard
 
         private void CreateClient_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            ServerIpValue = ServerIp.Text;
         }
+
+        
     }
 }
